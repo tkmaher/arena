7f0897b4a819ae0af17ec8c636538d57ac28b67b
tkmaher <tkmaher@umich.edu>
2026-04-09 14:22:24 -0500

    deploy this fool

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.are.na", "s3.amazonaws.com", "images.are.na", "d2w9rnfcy7mm78.cloudfront.net"],
+    remotePatterns: [new URL("cdn.are.na"), new URL("s3-us-west-2.amazonaws.com/arena-attachments"),
+    new URL("cdn.are.na"), new URL("s3-us-west-2.amazonaws.com/arena-attachments")],
+    unoptimized: true,
   },
   output: "export"
+  
 };
 
 export default nextConfig;
