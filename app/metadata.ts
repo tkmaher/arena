8f0f7151f5c826200ccb6842ed56b16c748e0797
tkmaher <tkmaher@umich.edu>
2026-04-11 17:26:53 -0500

    meta data

---
+++ app/metadata.ts
@@ -0,0 +1,47 @@
+import { Metadata } from "next";
+
+export const staticMetadata: Metadata = {
+    metadataBase: new URL("https://arena-flow.org/"),
+    applicationName: "arena flow",
+    title: {
+      template: 'arena flow | %s',
+      default: 'arena flow', // a default is required when creating a template
+    },
+    description: "arena-flow.org is a directed graph of are.na. Click the canvas to add a node. Click a node to see its parents and children.",
+    keywords: ["are.na", "content-aggregator", "pinterest", "visuals", "images", "media"],
+    robots: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1,
+      googleBot: "index, follow"
+    },
+    openGraph: {
+      locale: "en_US",
+      siteName: "arena flow",
+      url: "https://arena-flow.org/",
+      type: "website",
+      images: [
+        {
+          url: "https://arena-flow.org/logotext.svg",
+          width: 1200,
+          height: 630,
+          alt: "arena flow"
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "arena flow",
+      
+      images: [
+        {
+          url: "https://arena-flow.org/",
+          width: 1200,
+          height: 630,
+          alt: "arena flow"
+        }
+      ]
+    }
+  };
