5381dfbd2841b8e5bf98f8f534c09fafdb686feb
tkmaher <tkmaher@umich.edu>
2026-04-13 12:45:51 -0500

    attribution

--- app/metadata.ts
+++ app/metadata.ts
@@ -19,7 +19,7 @@ export const staticMetadata: Metadata = {
     },
     openGraph: {
       locale: "en_US",
-      siteName: "arena flow",
+      siteName: "arena flow | A directed graph of are.na",
       url: "https://arena-flow.org/",
       type: "website",
       images: [
@@ -33,7 +33,7 @@ export const staticMetadata: Metadata = {
     },
     twitter: {
       card: "summary_large_image",
-      title: "arena flow",
+      title: "arena flow | A directed graph of are.na",
       
       images: [
         {
